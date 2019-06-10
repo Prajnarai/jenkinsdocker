55c48b3052ca94d38de5362c36d1b54554a26fa0
prajnarai <prajna.shetty12@gmail.com>
2019-06-10 17:33:31 -0500

    reupdate

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 
 FROM centos
-RUN apt-get update
-RUN apt-get -y install httpd
+RUN yum -y install httpd
 ADD . /var/www/html
 ENTRYPOINT httpd -D FOREGROUND
 ENV name Devops
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker rm -f $(sudo docker ps -a -q)
+docker rm -f $(docker ps -a -q)
 docker rmi newproject
 docker build . -t newproject
 docker run -it -p 82:80 -d newproject
